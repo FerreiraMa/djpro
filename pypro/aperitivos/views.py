3d247da242cf07035211e805a07bd56fa87f889c
FerreiraMa <ferreirasantos1976@uol.com.br>
2021-04-12 19:47:44 -0300

    Segue abaixo a lista com as implementações: 01) Atualizado o arquivo test_video.py; 02) Atualizado o arquivo test_indice.py; 03) Atualizado a views.py; 04) Atualizado o models.py; 05) Criado o arquivo admin.py 06) Instalado o mommy

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,23 +1,14 @@
-from django.shortcuts import render
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from pypro.aperitivos.models import Video
 
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
-
-videos = [
-    Video('motivacao', 'Video Aperitivo: Motivação',  422595352),
-    Video('instalacao-windows', 'Instalação Windows', 251497668),
-]
 
 """
+videos = [
+    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id=422595352),
+    Video(slug='instalacao-windows', titulo='Instalação Windows', vimeo_id=251497668),
+]
+
+
 Lista com os vídeos mostrados no html, foi alterada para acessar de um objeto (Video) dentro do contexto de templates
 videos = [
     {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 422595352},
@@ -25,15 +16,29 @@ videos = [
 ]
 
 videos_dct = {dct['slug']: dct for dct in videos}
-"""
 
 videos_dct = {v.slug: v for v in videos}
+"""
 
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
+    video = get_object_or_404(Video, slug=slug)
+    return render(request, 'aperitivos/video.html', context={'video': video})
+
+
+"""
+Função utilizada para carregar os dados da lista videos_dct.
+def video(request, slug):
     video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
+
+Foi inserido o get_object_or_404 para casos onde o vídeo não é encontrado
+def video(request, slug):
+    video = Video.objects.get(slug=slug)
+    return render(request, 'aperitivos/video.html', context={'video': video})
+"""
